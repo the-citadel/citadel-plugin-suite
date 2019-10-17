aa4ab1f7e883ab5f6a0ce6d19383cd3fad6f29b7
Morgan Spencer <mspence4@citadel.edu>
2019-10-17 18:12:08 -0400

    Gutenberg Overlay: removed JS temporarily, added border around title, moved block names to upper right of block

--- gutenberg-overlay/js/scripts.js
+++ gutenberg-overlay/js/scripts.js
@@ -1,20 +1,5 @@
 jQuery(document).ready(function($) {
 
 
-	$('body.block-editor-page').click( function() {
-
-		if ($(this).find('.editor-block-list__block.is-selected').length !== 0) {
-
-			$('#editor').addClass('block-selected');
-
-		} else {
-
-			$('#editor').removeClass('block-selected');
-
-		}
-
-	});
-
-	$('.edit-post-layout__metaboxes:not(:empty):first').addClass('not-empty');
 
 });
