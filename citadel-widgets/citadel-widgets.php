e6b5569164f6e4546ead798485272259e7741960
Morgan Spencer <mspence4@citadel.edu>
2019-10-24 17:16:19 -0400

    Citadel Widgets: removed all default widgets

---
+++ citadel-widgets/citadel-widgets.php
@@ -0,0 +1,26 @@
+<?php
+
+// Remove default widgets
+function remove_widgets() {
+
+	unregister_widget('WP_Nav_Menu_Widget');
+    unregister_widget('WP_Widget_Archives');
+	unregister_widget('WP_Widget_Calendar');
+	unregister_widget('WP_Widget_Categories');
+	unregister_widget('WP_Widget_Custom_HTML');
+	unregister_widget('WP_Widget_Links');
+	unregister_widget('WP_Widget_Media_Audio');
+	unregister_widget('WP_Widget_Media_Image');
+	// unregister_widget('WP_Widget_Media_Video');
+	unregister_widget('WP_Widget_Media_Gallery');
+	unregister_widget('WP_Widget_Meta');
+	unregister_widget('WP_Widget_Pages');
+	unregister_widget('WP_Widget_Recent_Comments');
+	unregister_widget('WP_Widget_Recent_Posts');
+	unregister_widget('WP_Widget_RSS');
+	unregister_widget('WP_Widget_Search');
+	unregister_widget('WP_Widget_Tag_Cloud');
+	unregister_widget('WP_Widget_Text');
+
+}
+add_action('widgets_init', 'remove_widgets');
