9401d373537b833af603da6a2dfeab8858a7f1e3
Morgan Spencer <mspence4@citadel.edu>
2019-10-24 17:18:59 -0400

    Outdated Notifications: reformatted layout when no posts are outdated

--- outdated-notifications/php/admin.php
+++ outdated-notifications/php/admin.php
@@ -2,120 +2,117 @@
 
 defined( 'ABSPATH' ) || exit;
 
-?>
+ 
+// the query
+$paged = isset($_GET['paged']) ? $_GET['paged'] : 1; 
+$args = array(
+	'post_type'			=> 'page',
+	'post_status'		=> 'publish',
+	'posts_per_page'	=> 20,
+	'paged'				=> $paged,
+	'orderby'			=> 'modified',
+	'order' 			=> 'ASC',
+	'date_query' 		=> array(
+							array(
+								'column' 	=> 'post_modified',
+								'before'  	=> '3 months ago',
+							),
+	),
+);
 
-<div class="wrap">
-	<h1 class="wp-heading-inline">Outdated Pages</h1>
-	<hr class="wp-header-end">
+$custom_query = new WP_Query($args); ?>
+	
+	<?php if ( $custom_query->have_posts() ) : ?>
 
-	<?php 
-	// the query
-	$paged = isset($_GET['paged']) ? $_GET['paged'] : 1; 
-	$args = array(
-		'post_type'			=> 'page',
-		'post_status'		=> 'publish',
-		'posts_per_page'	=> 25,
-		'paged'				=> $paged,
-		'orderby'			=> 'modified',
-		'order' 			=> 'ASC',
-		'date_query' 		=> array(
-								array(
-									'column' => 'post_modified',
-									'before'  => '3 months ago',
-								),
-		),
-	);
+	<table class="outdated-pages wp-list-table widefat fixed striped pages">
+		<thead>
+			<tr>
+				<th>Page Title</th>
+				<th>Last Updated</th>
+				<th>Actions</th>
+			</tr>
+		</thead>
+		<tbody>
 
-	$custom_query = new WP_Query($args); ?>
+		<!-- the loop -->
+		
+		<?php while ( $custom_query->have_posts() ) : $custom_query->the_post(); ?>
 
-		<table class="outdated-pages wp-list-table widefat fixed striped pages">
-			<thead>
-				<tr>
-					<th>Page Title</th>
-					<th>Last Updated</th>
-					<th>Actions</th>
-				</tr>
-			</thead>
-			<tbody>
+			<tr>
+				<td><strong><a href="<?php echo the_permalink(); ?>" target="_blank" title="View '<?php the_title(); ?>'"><?php the_title(); ?></a></strong></td>
+				<td><?php the_modified_date(); ?></td>
+				<td class="actions">
+					<a href="<?php echo the_permalink(); ?>" title="View '<?php the_title(); ?>'"><i class="fas fa-fw fa-eye"></i></a>
+					<a href="<?php echo get_edit_post_link(); ?>" title="Edit '<?php the_title(); ?>'"><i class="fas fa-fw fa-pencil-alt"></i></a>
+				</td>
+			</tr>
 
-			<!-- the loop -->
-			<?php if ( $custom_query->have_posts() ) :  while ( $custom_query->have_posts() ) : $custom_query->the_post(); ?>
+		<?php endwhile; ?>
+		<!-- end of the loop -->
 
-				<tr>
-					<td><strong><a href="<?php echo the_permalink(); ?>" target="_blank" title="View '<?php the_title(); ?>'"><?php the_title(); ?></a></strong></td>
-					<td><?php the_modified_date(); ?></td>
-					<td class="actions">
-						<a href="<?php echo the_permalink(); ?>" title="View '<?php the_title(); ?>'"><i class="fas fa-fw fa-eye"></i></a>
-						<a href="<?php echo get_edit_post_link(); ?>" title="Edit '<?php the_title(); ?>'"><i class="fas fa-fw fa-pencil-alt"></i></a>
-					</td>
-				</tr>
+		</tbody>
+		<tfoot>
+			<tr>
+				<th>Page Title</th>
+				<th>Last Updated</th>
+				<th>Actions</th>
+			</tr>
+		</tfoot>
+	</table>
 
-			<?php endwhile; ?>
-			<?php else : ?>
-				<tr>
-					<td colspan="3"><?php _e( 'There are no outdated pages. Great job!' ); ?></td>
-				</tr>
-			<?php endif; ?>
-			<!-- end of the loop -->
+	<div class="tablenav bottom">
 
-			</tbody>
-			<tfoot>
-				<tr>
-					<th>Page Title</th>
-					<th>Last Updated</th>
-					<th>Actions</th>
-				</tr>
-			</tfoot>
-		</table>
+		<div class="alignleft">
 
-		<div class="tablenav bottom">
-
-			<div class="alignleft">
-
-				<span class="pagination-links">
-					<?php
-						$big = 999999999; // need an unlikely integer
-						echo paginate_links( array(
-						    'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-						    'format' => '?paged=%#%',
-						    'current' => max( 1, $paged ),
-						    'total' => $custom_query->max_num_pages
-						) );
-					?>
-				</span>
-				
-			</div>
-
-			<div class="tablenav-pages">
-
-				<span class="displaying-num">
-					<?php
-
-						$count = $custom_query->found_posts;
-
-						echo $count . ' items';
-					?>
-				</span>
-				
-			</div>
-
+			<span class="pagination-links">
+				<?php
+					$big = 999999999; // need an unlikely integer
+					echo paginate_links( array(
+					    'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+					    'format' => '?paged=%#%',
+					    'current' => max( 1, $paged ),
+					    'total' => $custom_query->max_num_pages
+					) );
+				?>
+			</span>
+			
 		</div>
 
-		<?php wp_reset_postdata(); ?>
+		<div class="tablenav-pages">
 
-		<style type="text/css">
-			.outdated-pages {
-				margin-top: 10px;
-			}
+			<span class="displaying-num">
+				<?php
 
-			.outdated-pages .actions a {
-				font-size: 1.2em;
-				margin-right: 10px;
-			}
+					$count = $custom_query->found_posts;
 
-			.page-numbers {
-				margin-right: 3px;
-			}
-		</style>
+					echo $count . ' items';
+				?>
+			</span>
+			
+		</div>
 
-</div>
+	</div>
+
+	<?php else : ?>
+
+		<p><?php _e( 'There are no outdated pages. Great job!' ); ?></p>
+
+	<?php endif; ?>
+
+	<?php wp_reset_postdata(); ?>
+
+	<style type="text/css">
+		.outdated-pages {
+			margin-top: 10px;
+		}
+
+		.outdated-pages .actions a {
+			font-size: 1.2em;
+			margin-right: 10px;
+		}
+
+		.page-numbers {
+			margin-right: 3px;
+		}
+	</style>
+
