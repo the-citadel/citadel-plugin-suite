4dfb8e298f569d5846424a5bebaed124cd66a4f9
Morgan Spencer <mspence4@citadel.edu>
2019-10-24 09:41:24 -0400

    Preview Notifications: highlight images that don't have alt text

--- preview-notification/js/scripts.js
+++ preview-notification/js/scripts.js
@@ -1,7 +1,32 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready( function($) {
 
-	$message = 'This is a preview, not the live page.';
+	$('.entry-content img').each( function() {
 
-	$('body').prepend('<div id="preview-message">' + $message + '</div>');
+		if ( ! $(this).attr('alt') ) {
+
+			$(this).wrap('<div class="cit-preview-alert cit-alt-alert"></div>');
+
+		}
+
+	});
+
+
+
+	var issueCount = $('.cit-preview-alert').length,
+		message = 'This is a preview, not the live page.';
+
+	if ( issueCount == 1 ) {
+
+		var issueMessage = 'There is 1 issue on this page.',
+			message = message + ' ' + issueMessage;
+	
+	} else if ( issueCount > 1 ) {
+
+		var issueMessage = 'There are ' + issueCount + ' issues on this page.',
+			message = message + ' ' + issueMessage;
+
+	}
+
+	$('body').prepend('<div id="preview-message">' + message + '</div>');
 
 });
