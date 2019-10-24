538ca60da83756e7fa69b8bfba766e07d6595b15
Morgan Spencer <mspence4@citadel.edu>
2019-10-24 17:19:27 -0400

    Preview Notification: added errors list

--- preview-notification/js/scripts.js
+++ preview-notification/js/scripts.js
@@ -10,23 +10,43 @@ jQuery(document).ready( function($) {
 
 	});
 
-
-
 	var issueCount = $('.cit-preview-alert').length,
+		altIssues = $('.cit-alt-alert').length,
 		message = 'This is a preview, not the live page.';
 
 	if ( issueCount == 1 ) {
 
-		var issueMessage = 'There is 1 issue on this page.',
+		var issueMessage = 'There is <button>1 issue</button> on this page.',
 			message = message + ' ' + issueMessage;
 	
 	} else if ( issueCount > 1 ) {
 
-		var issueMessage = 'There are ' + issueCount + ' issues on this page.',
+		var issueMessage = 'There are <button>' + issueCount + ' issues</button> on this page.',
 			message = message + ' ' + issueMessage;
 
 	}
 
-	$('body').prepend('<div id="preview-message">' + message + '</div>');
+	var errors = '';
+
+	if ( altIssues > 0 ) {
+		errors = errors + '<li><strong>(' + altIssues + ')</strong> Images missing alt text</li>';
+	}
+
+	if ( issueCount > 0 ) {
+
+		$('body').prepend('<div id="preview-message"><p>' + message + '</p><div class="errors"><ul>' + errors + '</ul></div></div>');
+
+	} else {
+
+		$('body').prepend('<div id="preview-message"><p>' + message + '</p></div>');
+
+	}
+
+	$('#preview-message button').click(function() {
+
+		$('#preview-message .errors').stop().slideToggle(400);
+
+	});
+	
 
 });
